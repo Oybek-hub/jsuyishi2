5b7711e03d2d350e332254764239ed04663a2b8f
Adi__jaan <Adi__jaan>
2022-03-02 10:51:23 +0500

    jsuyishi2

---
+++ js1.js
@@ -0,0 +1,120 @@
+// var ism = prompt("Ismingizni yozing")
+// while (!isNaN(ism)) {
+//     ism = prompt("Iltimos ismingizni yozing")
+
+// }
+
+// var yosh = +prompt("Yoshingizni kiriting")
+// while (isNaN(yosh)) {
+//     yosh = +prompt("Iltimos sizdan yoshingizni kiriting!!!")
+
+// }
+var a = 3
+var b = 4
+var c = a + b
+var yigindi = +prompt("Misolni ishlang 3 + 4")
+while (isNaN(yigindi)) {
+    yigindi = +prompt("Javobingizga son kiriting")
+
+}
+if (c == yigindi) {
+    console.log("Sizning javob to'g'ri: " + yigindi);
+
+}
+else {
+    console.log("Sizning javob afsuski noto'g'ri: " + yigindi + " To'g'ri javob: " + c);
+}
+
+
+
+
+var a = 5
+var b = 3
+var c = 5 - 3
+var yigindi = +prompt("Misolni ishlang 5 - 3")
+while (isNaN(yigindi)) {
+    yigindi = +prompt("Javobingizga son kiriting")
+
+}
+if (c == yigindi) {
+    console.log("Sizning javob to'g'ri: " + yigindi);
+
+}
+else {
+    console.log("Sizning javob afsuski noto'g'ri: " + yigindi + " To'g'ri javob: " + c);
+}
+
+
+
+
+var a = 8
+var b = 7
+var c = 8 * 7
+var yigindi = +prompt("Misolni ishlang 8 * 7")
+while (isNaN(yigindi)) {
+    yigindi = +prompt("Javobingizga son kiriting")
+
+}
+if (c == yigindi) {
+    console.log("Sizning javob to'g'ri: " + yigindi);
+
+}
+else {
+    console.log("Sizning javob afsuski noto'g'ri: " + yigindi + " To'g'ri javob: " + c);
+}
+
+
+var a = 130
+var b = 2
+var c = 130 / 2
+var yigindi = +prompt("Misolni ishlang 130 / 2")
+while (isNaN(yigindi)) {
+    yigindi = +prompt("Javobingizga son kiriting")
+
+}
+if (c == yigindi) {
+    console.log("Sizning javob to'g'ri: " + yigindi);
+
+}
+else {
+    console.log("Sizning javob afsuski noto'g'ri: " + yigindi + " To'g'ri javob: " + c);
+}
+
+
+
+var a = 180
+var b = 6
+var c = 180 % 6
+var yigindi = +prompt("Misolni ishlang 180 % 6 ")
+while (isNaN(yigindi)) {
+    yigindi = +prompt("Javobingizga son kiriting")
+
+}
+if (c == yigindi) {
+    console.log("Sizning javob to'g'ri: " + yigindi);
+
+}
+else {
+    console.log("Sizning javob afsuski noto'g'ri: " + yigindi + " To'g'ri javob: " + c);
+}
+
+var a = 181
+var b = 4
+var c = 181 / 4
+var yigindi = +prompt("Misolni ishlang 181 / 4 ")
+while (isNaN(yigindi)) {
+    yigindi = +prompt("Javobingizga son kiriting")
+
+}
+if (c == yigindi) {
+    console.log("Sizning javob to'g'ri: " + yigindi);
+
+}
+else {
+    console.log("Sizning javob afsuski noto'g'ri: " + yigindi + " To'g'ri javob: " + c);
+}
+
+
+
+
+
